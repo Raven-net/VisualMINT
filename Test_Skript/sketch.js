e6714818ae6cd467c4a29ce3c644729342426052
Steffen Ravekes <steffen.ravekes@gmx.de>
2021-10-20 16:32:10 +0200

    Sinus fertig

--- Test_Skript/sketch.js
+++ Test_Skript/sketch.js
@@ -2,12 +2,22 @@ let angle = 0;
 let sinus = [];
 let cosinus = [];
 let slider1;
+let button_sin;
+let button_cos;
+let s_on = false;
+let k_on = false;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  slider1 = createSlider(0, 0.1, 0, 0.005);
+  slider1 = createSlider(0, 0.1, 0.006, 0.002);
   slider1.position(20, 10);
   slider1.style('width', '150px');
+  button_sin = createButton('Sinus');
+  button_sin.position(40, 35);
+  button_sin.mousePressed(SinusOn);
+  button_cos = createButton('Kosinus');
+  button_cos.position(100, 35);
+  button_cos.mousePressed(KosinusOn);
 }
 
 function draw() {
@@ -23,8 +33,10 @@ function draw() {
 
   let x = radius * cos(angle);
   let y = radius * sin(angle);
-  sinus.unshift(y);
-  cosinus.unshift(-x);
+
+  sinus.push(y);
+  cosinus.push(-x);
+
   fill(255);
   line(0, 0, x, y);
   ellipse(x, y, 5);
@@ -34,12 +46,80 @@ function draw() {
   line(x, y, x, 0);
   stroke(255);
 
-  line(x, y, ww * 3 / 12, y);
+  line(-ww * 3 / 12, 0, ww * 3 / 12, 0);
+  line(0, -ww * 3 / 12, 0, ww * 3 / 12);
 
-  // hier fehlen weitere Inhalte
+  stroke(0, 255, 0);
+  line(x, 0, 0, 0);
+  stroke(255);
 
+  translate(ww * 3 / 12, 0);
+
+  line(0, 0, ww / 2, 0);
+  line(0, -ww * 3 / 12, 0, ww * 3 / 12);
+
+  if (s_on) {
+    noFill();
+    beginShape();
+    stroke(255, 0, 0);
+    for (i = 0; i < sinus.length; i++) {
+      vertex(i, sinus[i]);
+    }
+    endShape();
+    fill(255);
+    stroke(255, 0, 0);
+    line(2, y, 2, 0);
+    line(0, y, 0, 0);
+    stroke(255);
+    line(0, y, sinus.length, y);
+    ellipse(0, y, 5);
+  }
+
+  if (k_on) {
+    noFill();
+    beginShape();
+    stroke(0, 255, 0);
+    for (i = 0; i < cosinus.length; i++) {
+      vertex(i, cosinus[i]);
+    }
+    endShape();
+    fill(255);
+    stroke(0, 255, 0);
+    line(-2, -x, -2, 0);
+    line(0, -x, 0, 0);
+    stroke(255);
+    line(0, -x, cosinus.length, -x);
+    ellipse(0, -x, 5);
+  }
+
+  angle -= val;
+
+  if (sinus.length > ww / 2) {
+    sinus = [];
+    cosinus = [];
+    angle = 0
+  }
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+  sinus = [];
+  cosinus = [];
+  angle = 0
+}
+
+function SinusOn() {
+  if (s_on == false) {
+    s_on = true;
+  } else {
+    s_on = false;
+  }
+}
+
+function KosinusOn() {
+  if (k_on == false) {
+    k_on = true;
+  } else {
+    k_on = false;
+  }
 }
