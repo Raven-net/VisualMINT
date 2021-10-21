e5c79c3643c65759a99c30f2f609fa85a8331b19
Steffen Ravekes <steffen.ravekes@gmx.de>
2021-10-21 21:51:20 +0200

    mandel test

--- Mandelbrot/sketch.js
+++ Mandelbrot/sketch.js
@@ -7,12 +7,25 @@ function preload(){
 function setup() {
   createCanvas(1920, 1440);
   image(img, 0, 0);
+  background(200);
 }
 
+// maping
+// x: 0,25 == pixel 1554
+// x: -0,75 == pixel 928
+// x: 0 == pixel 1398
+// -> 1 == 626 pixel
+// y: 0 == pixel 719
+// y: 0,8 == 219
+// -> 1 == 625
+
 function draw() {
-  image(img, 0, 0);
+  // image(img, 0, 0);
   stroke(255);
-  translate(width / 2, height / 2);
-  line(-width / 2, 0, width / 2, 0);
+  translate(1398, 719);
+  line(-1398, 0, 1920 - 1398, 0);
+  line(0, -719, 0, 721);
+  line(928, -5, 928, 5);
+  line(1554, -5, 1554, 5);
   ellipse(0, 0, 5);
 }
