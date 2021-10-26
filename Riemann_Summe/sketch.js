0058df9bb58884d4d12d53b044dfc30ee032b14b
Steffen Ravekes <steffen.ravekes@gmx.de>
2021-10-26 09:58:08 +0200

    mandel finish

---
+++ Riemann_Summe/sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0);
+  translate(width / 2, height / 2);
+  stroke(255);
+  line(-width / 2, 0, width / 2, 0);
+  line(0, -height / 2, 0, height / 2);
+
+  noFill();
+  beginShape();
+  for (i = 0; i < width; i++) {
+    vertex(i - width / 2, -0.002*(i-width/2)**2);
+  }
+  endShape();
+}
