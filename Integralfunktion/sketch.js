dfaaf3e901e1ca9fc31dae99fab713ec97ffe83d
Steffen Ravekes <steffen.ravekes@gmx.de>
2021-11-14 10:15:22 +0100

    init Integralfunktion

--- Integralfunktion/sketch.js
+++ Integralfunktion/sketch.js
@@ -3,54 +3,32 @@ const sclX = 10,
 
 let distX;
 let distY;
-// let riemann_on = false;
-// let button;
 let a, b;
-let n;
-let sliderN;
 let input;
-let sel;
-let area = 0;
-let funk = '-0.3 * x ** 2 + 4';
+let funk = 'sin(x)';
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  input = createInput('-0.3 * x ** 2 + 4');
+  input = createInput('sin(x)');
   input.position(30, 30);
   button = createButton('Funktion');
   button.position(input.x + input.width, 30);
   button.mousePressed(defFunk);
-  sliderN = createSlider(0, 60, 0);
-  sliderN.position(30, 60);
-  sliderN.style('width', '200px');
-  sel = createSelect();
-  sel.position(input.x + input.width + button.width + 10, 30);
-  sel.option('Links');
-  sel.option('Rechts');
-  sel.selected('Links');
-  // button = createButton('Riemann');
-  // button.position(40, 40);
-  // button.mousePressed(riemannOnOff);
   distX = width / sclX;
   distY = height / sclY;
-  a = new Point(- width / 4, 0);
+  a = new Point(0, 0);
   b = new Point(width / 4, 0);
 }
 
 function draw() {
-  n = sliderN.value();
   background(0);
-  textSize(22);
   fill(0, 102, 153);
   stroke(0);
-  text('Fläche = ' + area, 50, height - 100);
   translate(width / 2, height / 2);
   stroke(255);
   graphAxis();
+  graphIntegral();
   graphFunc();
-  if (n > 0) {
-    graphRiemann();
-  }
   a.show();
   a.move();
   b.show();
@@ -61,7 +39,7 @@ function draw() {
 function graphFunc() {
   push();
   noFill();
-  scale(1, -1);
+  // scale(1, -1);
   strokeWeight(3);
   stroke(255, 103, 2);
   beginShape();
@@ -74,26 +52,22 @@ function graphFunc() {
   pop();
 }
 
-function graphRiemann() {
+function graphIntegral() {
   let dist = b.x - a.x;
   let x = a.x;
-  area = 0;
   push();
   fill(0, 102, 153, 100);
   scale(1, -1);
   strokeWeight(2);
   stroke(0, 102, 153);
-  for (i = 0; i < n; i++) {
+  beginShape();
+  for (i = map(a.x, -sclX / 2, sclX / 2, -width / 2, width / 2); i < map(b.x, -sclX / 2, sclX / 2, -width / 2, width / 2); i++) {
+    let x = map(i, -width / 2, width / 2, -sclX / 2, sclX / 2);
     let y = funk1(x);
-    if (sel.value() == 'Links') {
-      rect(x * distX, 0, map(dist / n, -sclX / 2, sclX / 2, -width / 2, width / 2), map(y, -sclY / 2, sclY / 2, -height / 2, height / 2));
-      area += dist / n * y;
-    } else if (sel.value() == 'Rechts') {
-      rect(x * distX, 0, map(dist / n, -sclX / 2, sclX / 2, -width / 2, width / 2), map(funk1(x + dist / n), -sclY / 2, sclY / 2, -height / 2, height / 2));
-      area += dist / n * funk1(x + dist / n);
-    }
-    x += dist / n;
+    vertex(i, map(y, -sclY / 2, sclY / 2, -height / 2, height / 2));
+    vertex(i, 0);
   }
+  endShape();
 }
 
 function graphAxis() {
@@ -139,14 +113,6 @@ function windowResized() {
   b = new Point(width / 4, 0);
 }
 
-// function riemannOnOff(){
-//   if (riemann_on) {
-//     riemann_on = false;
-//   } else {
-//     riemann_on = true;
-//   }
-// }
-
 class Point {
   constructor(x, y) {
     this.x = map(x, -width / 2, width / 2, -sclX / 2, sclX / 2);
@@ -173,7 +139,6 @@ class Point {
     if (this.x < map(x, 0, width, -sclX / 2, sclX / 2) + 0.2 && this.x > map(x, 0, width, -sclX / 2, sclX / 2) - 0.2 && this.y < map(y, -height / 2, height / 2, -sclY / 2, sclY / 2) + 0.2 && this.y < map(y, -height / 2, height / 2, -sclY / 2, sclY / 2) - 0.2) {
       this.dragging = true;
     }
-    // this.dragging = true;
   }
 
   notPressed() {
