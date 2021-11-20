97198b5ee44c76aceffd10ad928592b943b2bb52
Steffen Ravekes <steffen.ravekes@gmx.de>
2021-11-20 10:09:26 +0100

    orbit Verbesserungen

--- orbit/sketch.js
+++ orbit/sketch.js
@@ -3,38 +3,30 @@ let sun;
 let pos;
 let vel;
 let acc;
+let mouseOverSun = false;
 
-class Sun {
-  constructor (x, y) {
-    this.pos = createVector(x, y);
-  }
-  show () {
-    fill(color(200, 200, 0));
-    ellipse(this.pos.x, this.pos.y, 50);
-  }
-  move () {
-    if (mouseIsPressed) {
-      this.pos.set(mouseX, mouseY);
-    }
-  }
+
+
+function setup() {
+  let canvas = createCanvas(windowWidth, windowHeight);
+  canvas.parent("myp5");
+// Create  Mover(x, y, red, green, blue, mass)
+  mover1 = new Mover(4 * width / 10, 4 * height / 10, 100, 149, 237, 2);
+  mover2 = new Mover(6 * width / 10, 6 * height / 10, 100, 149, 237, 3);
+// Create  Sun(x, y)
+  sun = new Sun(width / 2, height / 2);
 }
 
-class Mover {
-  constructor (x, y, r, g, b, m) {
-    this.pos = createVector(x, y);
-    this.color = color(r, g, b);
-    this.mass = m;
-    this.vel = p5.Vector.random2D().setMag(3);
-  }
-  show () {
-    fill(this.color);
-    ellipse(this.pos.x, this.pos.y, this.mass * 10);
-  }
-  update () {
-    this.acc = p5.Vector.sub(sun.pos, this.pos);
-    this.acc.setMag(0.1);
-    this.vel.add(this.acc);
-    this.pos.add(this.vel);
+function draw() {
+  background(255);
+  mover1.update();
+  mover1.show();
+  mover2.update();
+  mover2.show();
+
+  sun.show();
+  if (mouseOverSun) {
+    sun.move();
   }
 }
 
@@ -53,24 +45,47 @@ function drawArrow(base, vec, length) {
   pop();
 }
 
-function setup() {
-  let canvas = createCanvas(windowWidth, windowHeight);
-  canvas.parent("myp5");
-// Create  Mover(x, y, red, green, blue, mass)
-  mover1 = new Mover(4 * width / 10, 4 * height / 10, 0, 200, 200, 2);
-  mover2 = new Mover(6 * width / 10, 6 * height / 10, 200, 0, 200, 3);
-// Create  Sun(x, y)
-  sun = new Sun(width / 2, height / 2);
+function mousePressed() {
+  if (mouseX < sun.pos.x + 25 && mouseX > sun.pos.x - 25 && mouseY < sun.pos.y + 25 && mouseY > sun.pos.y - 25) {
+    mouseOverSun = true;
+  } else {
+    mouseOverSun = false;
+  }
 }
 
-function draw() {
-  background(255);
-  mover1.show();
-  mover1.update();
-  mover2.show();
-  mover2.update();
-  drawArrow(mover1.pos, mover1.acc, 15);
-  drawArrow(mover2.pos, mover2.acc, 15);
-  sun.show();
-  sun.move();
+function mouseReleased() {
+  mouseOverSun = false;
+}
+
+class Sun {
+  constructor (x, y) {
+    this.pos = createVector(x, y);
+  }
+  show () {
+    fill(color(200, 200, 0));
+    ellipse(this.pos.x, this.pos.y, 50);
+  }
+  move () {
+    this.pos.set(mouseX, mouseY);
+  }
+}
+
+class Mover {
+  constructor (x, y, r, g, b, m) {
+    this.pos = createVector(x, y);
+    this.color = color(r, g, b);
+    this.mass = m;
+    this.vel = p5.Vector.random2D().setMag(3);
+  }
+  show () {
+    fill(this.color);
+    ellipse(this.pos.x, this.pos.y, this.mass * 10);
+    drawArrow(this.pos, this.acc, 20);
+  }
+  update () {
+    this.acc = p5.Vector.sub(sun.pos, this.pos);
+    this.acc.setMag(0.1);
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+  }
 }
